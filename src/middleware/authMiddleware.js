9111731fee8ab6d14a26e89167ee0a964a8023f7
Dalila <crdalila@gmail.com>
2025-04-23 20:06:35 +0200

    isAdmin middleware y modificadas las rutas

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,15 +1,14 @@
 import { verifyToken } from "../utils/token.js";
 
+// PARA SABER SI HAS INICIADO SESIÓN:
 function isLoggedInAPI(req,res,next){
-    const authorization  = req.headers.authorization;
-    console.log("authorization",authorization);
+    const authorization  = req.headers.authorization; //para conseguir el token, te devuelve BEARER ELTOKENENCUESTIÓN
     if(!authorization){
-        res.status(401).json({error:"You shall not pass"});
+        res.status(401).json({error:"No has iniciado sesión"});
     }
-    let token = authorization.split(" "); // si no hay bearer espacio fallaria
-    token = token.pop();
+    let token = authorization.split(" "); //para separar el BEARER del token en sí
+    token = token.pop(); //nos quedamos con el último, el token
     const result = verifyToken(token);
-    console.log("token verified",result);
     if(result){
         req.user = {
             user_id: result.user_id,
@@ -17,23 +16,24 @@ function isLoggedInAPI(req,res,next){
         }
         next();
     }else{
-        res.status(401).json({error:"You shall not pass"});
-    }
-}
-async function isSeller(req,res,next){
-    const user  = req.session.user;
-    if(!user){
-        return res.redirect("/login?error=You+are+not+logged+in")
-    }
-    if(user.role ==="seller"){
-        next();
-    }else{
-        return res.redirect("/login?error=You+are+not+a+seller")
+        res.status(401).json({error:"No has iniciado sesión"});
     }
 }
 
+// PARA SABER SI ERES ADMIN
+function isAdmin(req, res, next) {
+    const user = req.user; //esto ya se habrá chequeado en isLoggedIn
+    if (!user) {
+        return res.status(401).json({ error: "No has iniciado sesión" });
+    }
+    if (user.role === "admin") {
+        return next();
+    } else {
+        return res.status(403).json({ error: "Acceso denegado: Tienes que ser admin para hacer esta operación" });
+    }
+}
 
 export {
-    isSeller,
-    isLoggedInAPI
+    isLoggedInAPI,
+    isAdmin,
 }
--- src/routes/categoryAPIRouter.js
+++ src/routes/categoryAPIRouter.js
@@ -1,16 +1,16 @@
 import {Router} from "express";
 import categoryAPIController from "../controllers/category/categoryAPIController.js";
-import { isLoggedInAPI } from "../middleware/authMiddleware.js";
+import { isLoggedInAPI, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", categoryAPIController.getAll);
 
-router.post("/crear", isLoggedInAPI, categoryAPIController.create);
+router.post("/crear", isLoggedInAPI, isAdmin, categoryAPIController.create);
 
-router.delete("/:id/eliminar", isLoggedInAPI,categoryAPIController.remove);
+router.delete("/:id/eliminar", isLoggedInAPI, isAdmin, categoryAPIController.remove);
 
-router.put("/:id", isLoggedInAPI, categoryAPIController.edit);
+router.put("/:id", isLoggedInAPI, isAdmin, categoryAPIController.edit);
 
 router.get("/:id", categoryAPIController.getByID);
 
